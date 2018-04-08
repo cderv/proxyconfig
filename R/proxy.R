44ab395f5206580ffdc0f81be395738ab1cb2f49
Christophe Dervieux <christophe.dervieux@gmail.com>
2018-04-08 18:56:33 +0200

    use package config to configure proxy url

--- R/proxy.R
+++ R/proxy.R
@@ -1,4 +1,15 @@
 
+get_proxy <- function(proxy_name) {
+  # get configuration from local yaml file
+  if (file.exists("proxyconfig.yml")) {
+    if (!requireNamespace("config", quietly = TRUE)) {
+      stop("you must installed the config package to use proxyconfig.yml configuration", call. = FALSE)
+    }
+    proxy <- config::get(proxy_name, file = "proxyconfig.yml")
+    return(proxy)
+  }
+  stop("proxy must be define through use_proxy argument or with a proxyconfig.yml file", call. = FALSE)
+}
 
 check_proxy <- function(proxy) {
   if (length(proxy) == 0) return(FALSE)
