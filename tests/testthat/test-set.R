0aa2672e5c165b1ba853be128e2912ca39f1f92e
Christophe Dervieux <christophe.dervieux@gmail.com>
2018-04-08 19:31:05 +0200

    add test to set_proxy

--- tests/testthat/test-set.R
+++ tests/testthat/test-set.R
@@ -1,5 +1,11 @@
 context("test-set.R")
 
+test_that("set_proxy stop if wrong proxy", {
+  expect_error(set_proxy())
+  expect_error(set_proxy(proxy = NULL))
+  expect_error(set_proxy(proxy = "34#YU"))
+})
+
 test_that("build_http_env correctly", {
   expect_equal(build_http_env("http://proxy-url.com:3232", "its", "me"),
                "http://its:me@proxy-url.com:3232/")
